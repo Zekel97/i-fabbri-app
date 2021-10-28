0337dd5d99fef878881e967b2f8d1a15c196d226
Zekel97 <szandreas97@gmail.com>
2021-10-28 19:39:51 +0200

    Implemented barebone header

---
+++ frontend/src/assets/components/Header.js
@@ -0,0 +1,26 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header>
+        <div class="title">
+          <h1>I FABBRI</h1>
+          <span>srl</span>
+        </div>
+
+         <div class="menu">
+          <div class="burger"></div>
+          <div class="nav">
+            <ul>
+              <li><a href="#">Homepage</a></li>
+              <li><a href="#">Residenziale</a></li>
+              <li><a href="#">Industriale</a></li>
+              <li><a href="#">Contatti</a></li>
+            </ul>
+          </div>
+         </div>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
 import App from './assets/pages/App';
+import Header from './assets/components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
