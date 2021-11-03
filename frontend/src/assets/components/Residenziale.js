f49e1fc18e0814510f05ce1397a0570eebfe98b8
Gabriela Veliu <gabriela.veliu2@gmail.com>
2021-11-03 17:22:32 +0100

    Initialization Residenziale page

---
+++ frontend/src/assets/components/Residenziale.js
@@ -0,0 +1,33 @@
+import '../styles/Residenziale.css';
+
+function Residenziale() {
+    return(
+        <div class="residenziale">
+            <div class="title">
+            <h1>RESIDENZIALE</h1>
+
+            </div>
+                <div class="photo-grid">
+
+                <figure class="photo photo-1">
+                    <figcaption><a href="#">1</a></figcaption>
+                </figure>
+                <figure class="photo photo-2">
+                    <figcaption><a href="#">2</a></figcaption>
+                </figure>
+                <figure class="photo photo-3">
+                    <figcaption><a href="#">3</a></figcaption>
+                </figure >
+                <figure class="photo photo-4">
+                    <figcaption><a href="#">4</a></figcaption>
+                </figure >
+                <figure class="photo photo-5">
+                    <figcaption><a href="#">5</a></figcaption>
+                </figure >
+            </div>
+        </div>
+
+    );
+}
+
+export default Residenziale;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/**import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";**/
 import './assets/styles/index.css';
 import App from './assets/pages/App';
 import Header from './assets/components/Header';
 import Homepage from './assets/components/Homepage';
 import Footer from './assets/components/Footer';
+import Residenziale from './assets/components/Residenziale';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <Homepage />
+    <Residenziale />
     <Footer />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
