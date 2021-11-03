6737c68f39496a90c35a1d3c6f92c5ab3eae4ff9
Gabriela Veliu <gabriela.veliu2@gmail.com>
2021-11-03 11:08:38 +0100

    Initialization footer

---
+++ frontend/src/assets/components/Footer.js
@@ -0,0 +1,32 @@
+import '../styles/Footer.css';
+
+function Footer() {
+    return(
+        <footer>
+            <div class="title">
+                <h1>I Fabbri</h1>
+                <span>srl</span>
+            </div>
+            <div class="footer-body">
+                <p>
+                    I Fabbri srl
+                </p>
+                <p>
+                    Contrada San Sisto, 2
+                </p>
+                <p>
+                    63816 Francavilla d'Ete (FM)
+                </p>
+                <p>
+                    Telefono: 0734.873010
+                </p>
+                <p>
+                    C.F. e P.Iva 02238200444
+                </p>
+            </div>
+        
+        </footer>
+    );
+}
+
+export default Footer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import './assets/styles/index.css';
 import App from './assets/pages/App';
 import Header from './assets/components/Header';
 import Homepage from './assets/components/Homepage';
+import Footer from './assets/components/Footer';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Homepage />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
